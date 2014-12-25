44f561b3829e89d6cd8eaa9c44d1e0cedd27840e
pylerSM <pyler@azet.sk>
2014-12-25 16:17:38 +0100

    Fully rewritten

--- src/com/pyler/xinternalsd/Preferences.java
+++ src/com/pyler/xinternalsd/Preferences.java
@@ -1,21 +1,39 @@
 package com.pyler.xinternalsd;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
 
-public class Preferences extends PreferenceActivity  {
+public class Preferences extends PreferenceActivity {
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
-		addPreferencesFromResource(R.xml.prefs);
-		Preference sdCardFullAccess = getPreferenceScreen()
-				.findPreference("sdcard_full_access");
-		boolean isKitKatOrNewer = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) ? true
-				: false;
-		sdCardFullAccess.setEnabled(isKitKatOrNewer);
+		addPreferencesFromResource(R.xml.preferences);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		PreferenceCategory appSettings = (PreferenceCategory) findPreference("app_settings");
+		Preference sdCardFullAccess = getPreferenceScreen().findPreference(
+				"sdcard_full_access");
+		EditTextPreference internalSdPath = (EditTextPreference) getPreferenceScreen()
+				.findPreference("internal_sd_path");
+
+		String extStorage = System.getenv("SECONDARY_STORAGE");
+		if (extStorage != null && !extStorage.isEmpty()) {
+			String externalSd = extStorage.split(":")[0];
+			internalSdPath.setSummary(externalSd);
+			internalSdPath.setText(externalSd);
+			prefs.edit().putString("internal_sd_path", externalSd).apply();
+		}
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+			appSettings.removePreference(sdCardFullAccess);
+		}
 	}
 }
--- src/com/pyler/xinternalsd/XInternalSD.java
+++ src/com/pyler/xinternalsd/XInternalSD.java
@@ -12,290 +12,191 @@ import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedHelpers;
 
 public class XInternalSD implements IXposedHookZygoteInit {
-	XSharedPreferences mPrefs;
-	boolean mEnabledModule;
-	boolean mEnabledForAllApps;
-	String mEnabledApps;
-	String mDisabledApps;
-	File mInternalSDCard;
-	File mAppFiles;
-	File mObbDir;
-	File mDownloadDir;
-	Context mContext;
-	String mPackageName;
+	public XSharedPreferences prefs;
+	public File internalSdPath;
+	public File appFilesPath;
+	public File obbDirPath;
+	public File downloadDirPath;
+	public Class<?> contextImpl = XposedHelpers.findClass(
+			"android.app.ContextImpl", null);
+	public Class<?> packageManagerService = XposedHelpers.findClass(
+			"com.android.server.pm.PackageManagerService", null);
+	public XC_MethodHook getExternalStorageDirectoryHook;
+	public XC_MethodHook getExternalFilesDirHook;
+	public XC_MethodHook getObbDirHook;
+	public XC_MethodHook getDownloadDirHook;
+	public XC_MethodHook externalSdCardAccessHook;
 
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable {
-		mPrefs = new XSharedPreferences(XInternalSD.class.getPackage()
-				.getName());
-		mPrefs.makeWorldReadable();
+		prefs = new XSharedPreferences(XInternalSD.class.getPackage().getName());
+		prefs.makeWorldReadable();
+
+		getExternalStorageDirectoryHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				String intSd = prefs.getString("internal_sd_path", "");
+				internalSdPath = new File(intSd);
+			}
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				if (isAppEnabled()) {
+					param.setResult(internalSdPath);
+				}
+			}
+
+		};
+
+		getExternalFilesDirHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				String arg = (String) param.args[0];
+				if (arg == null) {
+					String intSd = prefs.getString("internal_sd_path", "");
+					String appFiles = intSd + "/Android/data/"
+							+ AndroidAppHelper.currentPackageName() + "/files";
+					appFilesPath = new File(appFiles);
+
+				}
+			}
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				if (isAppEnabled()) {
+					param.setResult(appFilesPath);
+				}
+			}
+
+		};
+
+		getObbDirHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				String intSd = prefs.getString("internal_sd_path", "");
+				String obbDir = intSd + "/Android/obb/"
+						+ AndroidAppHelper.currentPackageName() + "/";
+				obbDirPath = new File(obbDir);
+
+			}
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				if (isAppEnabled()) {
+					param.setResult(obbDirPath);
+				}
+			}
+
+		};
+
+		getDownloadDirHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				boolean changeDownloadDirPath = prefs.getBoolean(
+						"change_download_path", true);
+				String type = (String) param.args[0];
+				if (Environment.DIRECTORY_DOWNLOADS.equals(type)
+						&& changeDownloadDirPath) {
+					@SuppressLint("SdCardPath")
+					String intSd = prefs.getString("internal_sd_path",
+							"/sdcard");
+					String downloadDir = intSd + "/Download";
+					downloadDirPath = new File(downloadDir);
+				}
+			}
+
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				if (isAppEnabled()) {
+					param.setResult(downloadDirPath);
+				}
+			}
+
+		};
+
+		externalSdCardAccessHook = new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param)
+					throws Throwable {
+				prefs.reload();
+				String permission = (String) param.args[1];
+				boolean sdCardFullAccess = prefs.getBoolean(
+						"sdcard_full_access", true);
+				if (sdCardFullAccess
+						&& (permission
+								.equals("android.permission.WRITE_EXTERNAL_STORAGE") || permission
+								.equals("android.permission.ACCESS_ALL_EXTERNAL_STORAGE"))) {
+					Class<?> process = XposedHelpers.findClass(
+							"android.os.Process", null);
+					int gid = (Integer) XposedHelpers.callStaticMethod(process,
+							"getGidForName", "media_rw");
+					Object settings = XposedHelpers.getObjectField(
+							param.thisObject, "mSettings");
+					Object permissions = XposedHelpers.getObjectField(settings,
+							"mPermissions");
+					Object bp = XposedHelpers.callMethod(permissions, "get",
+							permission);
+					int[] bpGids = (int[]) XposedHelpers.getObjectField(bp,
+							"gids");
+					XposedHelpers.setObjectField(bp, "gids",
+							appendInt(bpGids, gid));
+				}
+			}
+		};
+
 		XposedHelpers.findAndHookMethod(Environment.class,
-				"getExternalStorageDirectory", new XC_MethodHook() {
-					@Override
-					protected void beforeHookedMethod(MethodHookParam param)
-							throws Throwable {
-						mPrefs.reload();
-						mEnabledModule = mPrefs.getBoolean(
-								"custom_internal_sd", true);
-						mEnabledForAllApps = mPrefs.getBoolean(
-								"enable_for_all_apps", true);
-						@SuppressLint("SdCardPath")
-						String internalSD = mPrefs.getString(
-								"internal_sd_path", "/sdcard");
-						mInternalSDCard = new File(internalSD);
-						mContext = AndroidAppHelper.currentApplication();
-						if (mContext == null) {
-							mEnabledModule = false;
-						} else {
-							mPackageName = mContext.getPackageName();
-							if (mEnabledForAllApps) {
-
-								mDisabledApps = mPrefs.getString(
-										"disable_for_apps", "");
-								if (!mDisabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mDisabledApps
-											.contains(mPackageName) ? false
-											: true;
-
-								}
-							} else {
-								mEnabledApps = mPrefs.getString(
-										"enable_for_apps", "");
-								if (!mEnabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mEnabledApps
-											.contains(mPackageName) ? true
-											: false;
-
-								}
-							}
-						}
-
-					}
-
-					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
-						if (mEnabledModule) {
-							param.setResult(mInternalSDCard);
-						}
-					}
-
-				});
-		Class<?> mContextClass = XposedHelpers.findClass(
-				"android.app.ContextImpl", null);
-		XposedHelpers.findAndHookMethod(mContextClass, "getExternalFilesDir",
-				String.class, new XC_MethodHook() {
-					@Override
-					protected void beforeHookedMethod(MethodHookParam param)
-							throws Throwable {
-						mPrefs.reload();
-						mEnabledModule = mPrefs.getBoolean(
-								"custom_internal_sd", true);
-						mEnabledForAllApps = mPrefs.getBoolean(
-								"enable_for_all_apps", true);
-						String arg = (String) param.args[0];
-						if (arg != null) {
-							mEnabledModule = false;
-						} else {
-							@SuppressLint("SdCardPath")
-							String internalSD = mPrefs.getString(
-									"internal_sd_path", "/sdcard");
-							mContext = AndroidAppHelper.currentApplication();
-							if (mContext == null) {
-								mEnabledModule = false;
-							} else {
-								String appFiles = internalSD + "/Android/data/"
-										+ AndroidAppHelper.currentPackageName()
-										+ "/files";
-								mAppFiles = new File(appFiles);
-								mPackageName = mContext.getPackageName();
-								if (mEnabledForAllApps) {
-
-									mDisabledApps = mPrefs.getString(
-											"disable_for_apps", "");
-									if (!mDisabledApps.isEmpty()
-											&& mEnabledModule) {
-										mEnabledModule = mDisabledApps
-												.contains(mPackageName) ? false
-												: true;
-
-									}
-								} else {
-									mEnabledApps = mPrefs.getString(
-											"enable_for_apps", "");
-									if (!mEnabledApps.isEmpty()
-											&& mEnabledModule) {
-										mEnabledModule = mEnabledApps
-												.contains(mPackageName) ? true
-												: false;
-
-									}
-								}
-							}
-						}
-
-					}
-
-					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
-						if (mEnabledModule) {
-							param.setResult(mAppFiles);
-						}
-					}
-
-				});
-		XposedHelpers.findAndHookMethod(mContextClass, "getObbDir",
-				new XC_MethodHook() {
-					@Override
-					protected void beforeHookedMethod(MethodHookParam param)
-							throws Throwable {
-						mPrefs.reload();
-						mEnabledModule = mPrefs.getBoolean(
-								"custom_internal_sd", true);
-						mEnabledForAllApps = mPrefs.getBoolean(
-								"enable_for_all_apps", true);
-						@SuppressLint("SdCardPath")
-						String internalSD = mPrefs.getString(
-								"internal_sd_path", "/sdcard");
-						mContext = AndroidAppHelper.currentApplication();
-						if (mContext == null) {
-							mEnabledModule = false;
-						} else {
-							String obbDir = internalSD + "/Android/obb/"
-									+ AndroidAppHelper.currentPackageName()
-									+ "/";
-							mObbDir = new File(obbDir);
-							mEnabledApps = mPrefs.getString("enable_for_apps",
-									"");
-							mPackageName = mContext.getPackageName();
-							if (mEnabledForAllApps) {
-
-								mDisabledApps = mPrefs.getString(
-										"disable_for_apps", "");
-								if (!mDisabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mDisabledApps
-											.contains(mPackageName) ? false
-											: true;
-
-								}
-							} else {
-								mEnabledApps = mPrefs.getString(
-										"enable_for_apps", "");
-								if (!mEnabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mEnabledApps
-											.contains(mPackageName) ? true
-											: false;
-
-								}
-							}
-						}
-
-					}
-
-					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
-						if (mEnabledModule) {
-							param.setResult(mObbDir);
-						}
-					}
-
-				});
+				"getExternalStorageDirectory", getExternalStorageDirectoryHook);
+		XposedHelpers.findAndHookMethod(contextImpl, "getExternalFilesDir",
+				String.class, getExternalFilesDirHook);
+		XposedHelpers
+				.findAndHookMethod(contextImpl, "getObbDir", getObbDirHook);
 		XposedHelpers.findAndHookMethod(Environment.class,
 				"getExternalStoragePublicDirectory", String.class,
-				new XC_MethodHook() {
-					@Override
-					protected void beforeHookedMethod(MethodHookParam param)
-							throws Throwable {
-						mPrefs.reload();
-						String dirType = (String) param.args[0];
-						boolean isDownloadDir = Environment.DIRECTORY_DOWNLOADS
-								.equals(dirType) ? true : false;
-						mEnabledModule = mPrefs.getBoolean(
-								"custom_internal_sd", true);
-						boolean changeDownloadPath = mPrefs.getBoolean(
-								"change_download_path", true);
-						mEnabledForAllApps = mPrefs.getBoolean(
-								"enable_for_all_apps", true);
-						@SuppressLint("SdCardPath")
-						String internalSD = mPrefs.getString(
-								"internal_sd_path", "/sdcard");
-						mContext = AndroidAppHelper.currentApplication();
-						if (mContext == null || !isDownloadDir
-								|| !changeDownloadPath) {
-							mEnabledModule = false;
-						} else {
-							String downloadDir = internalSD + "/Download/";
-							mDownloadDir = new File(downloadDir);
-							mPackageName = mContext.getPackageName();
-							if (mEnabledForAllApps) {
-
-								mDisabledApps = mPrefs.getString(
-										"disable_for_apps", "");
-								if (!mDisabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mDisabledApps
-											.contains(mPackageName) ? false
-											: true;
-
-								}
-							} else {
-								mEnabledApps = mPrefs.getString(
-										"enable_for_apps", "");
-								if (!mEnabledApps.isEmpty() && mEnabledModule) {
-									mEnabledModule = mEnabledApps
-											.contains(mPackageName) ? true
-											: false;
-
-								}
-							}
-						}
-
-					}
-
-					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
-						if (mEnabledModule) {
-							param.setResult(mDownloadDir);
-						}
-					}
-
-				});
-		Class<?> packageManagerService = XposedHelpers.findClass(
-				"com.android.server.pm.PackageManagerService", null);
+				getDownloadDirHook);
 		XposedHelpers.findAndHookMethod(packageManagerService,
-				"readPermission", "org.xmlpull.v1.XmlPullParser",
-				"java.lang.String", new XC_MethodHook() {
-					@Override
-					protected void afterHookedMethod(MethodHookParam param)
-							throws Throwable {
-						mPrefs.reload();
+				"readPermission", "org.xmlpull.v1.XmlPullParser", String.class,
+				externalSdCardAccessHook);
+	}
 
-						String permission = (String) param.args[1];
-						boolean sdCardFullAccess = mPrefs.getBoolean(
-								"sdcard_full_access", true);
-						if (sdCardFullAccess
-								&& (permission
-										.equals("android.permission.WRITE_EXTERNAL_STORAGE") || permission
-										.equals("android.permission.ACCESS_ALL_EXTERNAL_STORAGE"))) {
-							Class<?> process = XposedHelpers.findClass(
-									"android.os.Process", null);
-							int gid = (Integer) XposedHelpers.callStaticMethod(
-									process, "getGidForName", "media_rw");
-							Object mSettings = XposedHelpers.getObjectField(
-									param.thisObject, "mSettings");
-							Object mPermissions = XposedHelpers.getObjectField(
-									mSettings, "mPermissions");
-							Object bp = XposedHelpers.callMethod(mPermissions,
-									"get", permission);
-							int[] bpGids = (int[]) XposedHelpers
-									.getObjectField(bp, "gids");
-							XposedHelpers.setObjectField(bp, "gids",
-									appendInt(bpGids, gid));
-						}
-					}
-				});
+	public boolean isAppEnabled() {
+		boolean isAppEnabled = true;
+		boolean moduleEnabled = prefs.getBoolean("custom_internal_sd", true);
+		if (!moduleEnabled) {
+			return false;
+		}
+		Context context = AndroidAppHelper.currentApplication();
+		if (context == null) {
+			return false;
+		}
+		String packageName = AndroidAppHelper.currentPackageName();
+		boolean enabledForAllApps = prefs.getBoolean("enable_for_all_apps",
+				true);
+		if (enabledForAllApps) {
+			String disabledApps = prefs.getString("disable_for_apps", "");
+			if (!disabledApps.isEmpty()) {
+				isAppEnabled = disabledApps.contains(packageName) ? false
+						: true;
+			}
+		} else {
+			String enabledApps = prefs.getString("enable_for_apps", "");
+			if (!enabledApps.isEmpty()) {
+				isAppEnabled = enabledApps.contains(packageName) ? true : false;
+			}
+		}
+		return isAppEnabled;
+
 	}
 
 	public int[] appendInt(int[] cur, int val) {
